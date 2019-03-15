87240a47244dcff5346525594a8fb36ec01e9c01
wang.xuefei <wang.xuefei@immomo.com>
2019-03-15 17:53:28 +0800

    initProject

---
+++ TestZip/TestZip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestZip
+//
+//  Created by wangxuefei on 2019/3/15.
+//  Copyright © 2019年 MOMO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestZip/TestZip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestZip
+//
+//  Created by wangxuefei on 2019/3/15.
+//  Copyright © 2019年 MOMO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
