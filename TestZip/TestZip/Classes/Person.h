bc14d64a326172b646312e4c3bd3f6e409b71bca
wang.xuefei <wang.xuefei@immomo.com>
2019-03-15 18:04:23 +0800

    test

---
+++ TestZip/TestZip/Classes/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  TestZip
+//
+//  Created by wangxuefei on 2019/3/15.
+//  Copyright © 2019年 MOMO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@property (nonatomic, strong) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestZip/TestZip/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestZip
+//
+//  Created by wangxuefei on 2019/3/15.
+//  Copyright © 2019年 MOMO. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
